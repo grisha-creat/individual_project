58015446f83c38098dfca093e3dd243a8b9c6484
Grisha <zinovevgrisha@gmail.com>
2026-02-24 22:06:45 +0300

    second

--- algonearn_project/algonearn_project/settings.py
+++ algonearn_project/algonearn_project/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'learn',
-    'practice',
-    'users',
+    'main'     
 ]
 
 MIDDLEWARE = [
